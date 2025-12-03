bd42e472cacf9aa95544781c9835666119774c10
Abhiram-Mupparaju <amupp001@ucr.edu>
2025-12-03 02:16:42 +0000

    add datasets

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+import random as rand
+from sklearn.model_selection import train_test_split
+from sklearn.manifold import TSNE
+
+small_dataset = np.loadtxt('small-test-dataset-2-2.txt')
+large_dataset = np.loadtxt('large-test-dataset-2.txt')
+# lines 2 - 11 copied from my assignments in CS171
 
 def stub_evaluation():
 
